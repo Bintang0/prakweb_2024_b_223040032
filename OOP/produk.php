b7b13d13cbc411895cd460bb9f4e89f001c05775
indomitable <116551261+Bintang0@users.noreply.github.com>
2024-10-10 21:03:39 +0700

    Property and Method

--- OOP/produk.php
+++ OOP/produk.php
@@ -3,9 +3,32 @@
 //komik
 //game
 
-class produk {
+class Produk {
+         public $judul = "judul",
+                $penulis = "penulis",
+                $penerbit = "penerbit",
+                $harga = 0;
+
+                 public function getLablel() {
+                    return "$this->penulis, $this->penerbit";
+                 }
 
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+
+$produk3 = new Produk();
+$produk3->judul= "Naruto";
+$produk3->penulis= "Masashi";
+$produk3->penerbit= "Shonen jump";
+$produk3->harga= "50.000";
+
+$produk4 = new Produk();
+$produk4->judul= "Atomic ";
+$produk4->penulis= "Mas Rudi";
+$produk4->penerbit= "Gramedia";
+$produk4->harga= "1.000";
+
+echo "Komik:" . $produk3->getLablel();
+echo"<br>";
+echo "Game:" . $produk4->getLablel();
+

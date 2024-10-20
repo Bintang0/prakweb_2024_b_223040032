9286f93f63c65e83b43a660284283c0f62c6a662
indomitable <116551261+Bintang0@users.noreply.github.com>
2024-10-20 15:46:28 +0700

    Controller

---
+++ MVC/app/controllers/About.php
@@ -0,0 +1,11 @@
+<?php 
+
+class About {
+
+    public function index($nama="Bin", $pekerjaan= "Dosen",) {
+        echo "Hallo, Nama saya $nama, saya adalah seorang $pekerjaan";
+}
+    public function page() {
+        echo "About/page";
+}
+}
